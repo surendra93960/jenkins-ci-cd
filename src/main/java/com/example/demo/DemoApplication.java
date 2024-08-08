3336d62d99c98ffdea14c5453e5dcc42430ded93
surendra reddy seelam <surendrareddyseelam08@gmail.com>
2024-08-08 13:37:58 +0530

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/api")
+public class DemoApplication {
+
+	@GetMapping("/greet")
+	public String greet(){
+		return "Hello coders!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
